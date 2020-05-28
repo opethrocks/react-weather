6dc36dc4fdbb5b17c6653cc0100b75536ea7f6a0
opethrocks <lebleu131@icloud.com>
2020-05-28 11:17:02 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import Logo from './components/Logo/Logo';
 import './App.css';
+import Forecast from "./components/Forecast/Forecast";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <h1>React Weather App</h1>
       </header>
+      <main>
+        <Forecast />
+      </main>
+      <footer>
+        Page created by Matt LeBleu
+      </footer>
     </div>
   );
 }
+
 
 export default App;
